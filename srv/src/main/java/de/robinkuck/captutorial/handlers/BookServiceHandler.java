807cee9b9145613f04f9dcc438ebb0f94216aebd
Kucki99 <robin.kuck@sap.com>
2020-11-04 21:11:30 +0000

    Change bookservice discount

--- srv/src/main/java/de/robinkuck/captutorial/handlers/BookServiceHandler.java
+++ srv/src/main/java/de/robinkuck/captutorial/handlers/BookServiceHandler.java
@@ -1,5 +1,7 @@
 package de.robinkuck.captutorial.handlers;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.stream.Stream;
 
 import com.sap.cds.services.cds.CdsService;
@@ -17,10 +19,17 @@ import cds.gen.de.robinkuck.bookshop.Book;
 @ServiceName(BookService_.CDS_NAME)
 public class BookServiceHandler implements EventHandler {
 
+    public static final double DISCOUNT = 0.9;
+
     @After(event = CdsService.EVENT_READ, entity = Books_.CDS_NAME)
     public void discountBooks(Stream<Book> books) {
+        String discountSuffix = " (-" + Math.round((1 - DISCOUNT) * 100) + "%)";
         books.filter(book -> book.getStock() > 1000)
-                .forEach(book -> book.setTitle(book.getTitle() + " (discounted)"));
+                .forEach(book -> {
+                    book.setTitle(book.getTitle() + discountSuffix);
+                    BigDecimal result = book.getPrice().multiply(new BigDecimal(DISCOUNT)).setScale(2, RoundingMode.HALF_UP);
+                    book.setPrice(result);
+                });
     }
 
 }
--- srv/src/test/java/de/robinkuck/captutorial/handlers/BookServiceHandlerTest.java
+++ srv/src/test/java/de/robinkuck/captutorial/handlers/BookServiceHandlerTest.java
@@ -2,6 +2,7 @@ package de.robinkuck.captutorial.handlers;
 
 import static org.junit.Assert.assertEquals;
 
+import java.math.BigDecimal;
 import java.util.stream.Stream;
 
 import org.junit.Before;
@@ -13,7 +14,7 @@ public class BookServiceHandlerTest {
 
 	private BookServiceHandler handler = new BookServiceHandler();
 	private Book book = Book.create();
-
+    private String discountSuffix = " (-" + Math.round((1 - BookServiceHandler.DISCOUNT) * 100) + "%)";
     
 	@Before
 	public void prepareBook() {
@@ -22,16 +23,20 @@ public class BookServiceHandlerTest {
 
 	@Test
 	public void testDiscount() {
-		book.setStock(1200);
+        book.setStock(1200);
+        book.setPrice(new BigDecimal(30.0));
 		handler.discountBooks(Stream.of(book));
-		assertEquals("title (discounted)", book.getTitle());
+        assertEquals("title" + discountSuffix, book.getTitle());
+        assertEquals(new BigDecimal(27).setScale(book.getPrice().scale()), book.getPrice());
 	}
 
 	@Test
 	public void testNoDiscount() {
-		book.setStock(100);
+        book.setStock(100);
+        book.setPrice(new BigDecimal(30));
 		handler.discountBooks(Stream.of(book));
-		assertEquals("title", book.getTitle());
-	}
+        assertEquals("title", book.getTitle());
+        assertEquals(new BigDecimal(30), book.getPrice());
+    }
 
 }
